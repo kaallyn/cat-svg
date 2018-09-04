70fa4ae5075b95af851d2836d0da6faba40e6ada
Katie Allyn <kaallyn@gmail.com>
2018-09-03 19:56:36 -0500

    heart eyes, small mouth

--- js/app.js
+++ js/app.js
@@ -1,25 +1,30 @@
 
+var animationSmallMouth = document.getElementById("animation-small-mouth"),
+    mouthPath = document.getElementById("mouth"),
 
-// button's value to svg function
+    heartsButton = $('#hearts');
+
+// button's value to svg function. Used for CSS animations
 $("button.faceOption").click(function() {
     var firedBtnVal = $(this).val();
     // alert(firedBtnVal);
     // replace current class with that of the firedBtnVal
     $('svg').attr("class", firedBtnVal);
-
+    
     $('svg').one('webkitAnimationEnd oanimationend msAnimationEnd animationend',   
     function(e) {
     
         // code to execute after animation ends - removing the class that caused the animation. this allows same animation to be fired twice.
-
         $('svg').removeAttr('class');
     });
-
-    // remove all classes to #container
-    // $('#container').removeAttr('class');
-    // add class named button's blue to #container
-    // $('#container').addClass(firedBtnVal+'Bg');
-    
 });
 
+heartsButton.click(function() {
+    animationSmallMouth.beginElement();
+    // mouth.addEventListener('endEvent',animationEnd,false);
+});
+
+// function animationEnd() {
+//    alert("bang!");
+// }
 
